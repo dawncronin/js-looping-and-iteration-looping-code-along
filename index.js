d48fbd5651d5d81e9c5a3155c4e10062773f1867
dawncronin <github email address>
2019-12-09 14:03:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}`)
+    };
+    return arr
+};
